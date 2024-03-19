883012773a6f831b4ebf336ee5e0d4225feef185
Bragaglia, Stefano <stefano.bragaglia@astrazeneca.com>
2024-03-19 22:31:48 +0000

    Minor cospetic changes

--- pyLomma/fast.py
+++ pyLomma/fast.py
@@ -24,28 +24,28 @@ class Graph:
         with open(as_filename(folder, url), 'r') as f:
             dataset = json.load(f)
             abb, tuples = dataset['kind_to_abbrev'], dataset['metaedge_tuples']
-            convert = {f"{abb[s]}{abb[r]}{'' if d == 'both' else '>'}{abb[t]}": r for s, t, r, d in tuples}
+            convert = {f'{abb[s]}{abb[r]}{'' if d == 'both' else '>'}{abb[t]}': r for s, t, r, d in tuples}
 
         url = 'https://github.com/hetio/hetionet/raw/main/hetnet/tsv/hetionet-v1.0-edges.sif.gz'
         download(folder, url, 'b4b2ab8890d403c2077a65e0f34fc4d9')
-        edges = pd.read_csv(as_filename(folder, url), sep="\t").drop_duplicates()
+        edges = pd.read_csv(as_filename(folder, url), sep='\t').drop_duplicates()
         edges.insert(2, 'kind', edges.pop('metaedge').apply(lambda x: convert[x]))
 
         url = 'https://github.com/hetio/hetionet/raw/main/hetnet/tsv/hetionet-v1.0-nodes.tsv'
         download(folder, url, 'd2789861ab5b977dcee3c23ac9c9f7ce')
-        nodes = pd.read_csv(as_filename(folder, url), sep="\t").drop_duplicates()
+        nodes = pd.read_csv(as_filename(folder, url), sep='\t').drop_duplicates()
         nodes = nodes.rename(columns={'id': 'name', 'name': 'label'})
 
         return Graph(edges, nodes)
 
     def __init__(self, edges: pd.DataFrame, nodes: pd.DataFrame):
-        assert list(edges.columns) == ["source", "target", "kind"]
-        assert list(nodes.columns) == ["name", "label", "kind"]
+        assert list(edges.columns) == ['source', 'target', 'kind']
+        assert list(nodes.columns) == ['name', 'label', 'kind']
         with benchmark('graph loaded'):
             self.graph = ig.Graph.DataFrame(edges, directed=False, vertices=nodes, use_vids=False)
 
     def get_examples(self, rel: str) -> pd.DataFrame:
-        with benchmark("extract examples"):
+        with benchmark('extract examples'):
             pos = pd.DataFrame([{
                 'source': self.graph.vs[e.source]['name'],
                 'target': self.graph.vs[e.target]['name'],
@@ -54,14 +54,16 @@ class Graph:
 
             srcs = pos[['source']].drop_duplicates()
             tgts = pos[['target']].drop_duplicates()
-            result = srcs.merge(tgts.assign(kind=rel), how="cross")
-            result = result.merge(pos, how="left", indicator='expected')
+            result = srcs.merge(tgts.assign(kind=rel), how='cross')
+            result = result.merge(pos, how='left', indicator='expected')
             result = result.loc[result.source != result.target].drop_duplicates()
             result.insert(0, 'expected', result.pop('expected').apply(lambda x: x == 'both'))
 
             return result
 
     def sample(self, examples: pd.DataFrame, length: int) -> tuple[bool, list[tuple[str, str, str]]] | None:
+        assert list(examples.columns) == ['expected', 'source', 'target', 'kind']
+
         try:
             expected, source, target, kind = examples.sample().values[0]
 
@@ -102,8 +104,8 @@ if __name__ == '__main__':
     random.seed(42)
     np.random.seed(42)
 
-    g = Graph.hetionet("../data")
-    ex = g.get_examples("treats")
+    g = Graph.hetionet('../data')
+    ex = g.get_examples('treats')
     train, test = train_test_split(ex, test_size=1 / 3, random_state=42, shuffle=True, stratify=ex.expected)
 
     while True:
